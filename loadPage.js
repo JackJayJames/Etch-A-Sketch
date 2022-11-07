896e2335b0905c47fe750554816c8a6b4c9f10c1
JackJayJames <115401650+JackJayJames@users.noreply.github.com>
2022-11-07 11:47:25 +0100

    created container
    created container or the divs

--- loadPage.js
+++ loadPage.js
@@ -1,1 +1,20 @@
 console.log("here");
+
+function createContainer()
+{
+    const container = document.createElement("div");
+    container.id = "container";
+    container.textContent = "tfczvgubhinj";
+    document.querySelector("body").appendChild(container);
+}
+
+function createDivs(rows, cols)
+{
+    
+}
+
+function loadPage()
+{
+    const container = createContainer();
+    const squares = createDivs(16, 16);
+}
