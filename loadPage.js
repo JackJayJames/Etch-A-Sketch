d94bd1dc84c7940b78eedec941c17221647cbcd1
JackJayJames <115401650+JackJayJames@users.noreply.github.com>
2022-11-07 23:48:02 +0100

    size button, erase button
    created size and erased button

--- loadPage.js
+++ loadPage.js
@@ -10,8 +10,8 @@ function createContainer()
 function get_size_element(rows, cols)
 {
     let arr = [];
-    arr.push(924/rows);
-    arr.push(924/cols);
+    arr.push(960/rows);
+    arr.push(960/cols);
     return arr;
 }
 
@@ -27,9 +27,10 @@ function createDivs(rows, cols)
         {
             let div = document.createElement("div");
             div.id = `${i}x${j}`;
+            div.classList.add("divs");
             div.style.minWidth = `${size[0]}px`
             div.style.minHeight = `${size[1]}px`;
-            div.textContent = `${i}x${j}`;
+            //div.textContent = `${i}x${j}`;
             container.appendChild(div);
         }
     }
@@ -43,10 +44,36 @@ function hoverEvent(id)
     div.style.background = "blue";
 }
 
-function loadPage()
+function loadPage(size)
 {
-    const container = createDivs(16, 16);
+    const container = createDivs(size, size);
     container.childNodes.forEach(MouseOver => MouseOver.addEventListener("mouseover", function (e) {
         hoverEvent(e.target.id)
     }));
 }
+
+function eraseDivs()
+{
+    const container = document.querySelector("#container");
+    container.innerHTML = "";
+}
+
+function resizedContainer()
+{
+    const size = prompt("Enter your size (entered number must be from 1 to 100)");
+    eraseDivs();
+    loadPage(size);
+}
+
+function revert_divs_color()
+{
+    const divs = document.querySelectorAll(".divs");
+    divs.forEach(div => div.style.background = "white");
+    //console.log(button);
+}
+
+const size_button = document.querySelector("#sizeButton");
+size_button.addEventListener("click", () => resizedContainer())
+
+const erase_button = document.querySelector("#eraseButton");
+erase_button.addEventListener("click", () => revert_divs_color());
