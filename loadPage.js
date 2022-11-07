fa92e62ff91bee135eab8814874a5830fa39d038
JackJayJames <115401650+JackJayJames@users.noreply.github.com>
2022-11-07 23:19:42 +0100

    div size

--- loadPage.js
+++ loadPage.js
@@ -7,16 +7,28 @@ function createContainer()
     return container;
 }
 
+function get_size_element(rows, cols)
+{
+    let arr = [];
+    arr.push(924/rows);
+    arr.push(924/cols);
+    return arr;
+}
+
 function createDivs(rows, cols)
 {
     const container = createContainer();
+    let size = get_size_element(rows, cols);
+    console.log(size);
     for(i = 0; i < cols; i++)
     {
         for(j = 0; j < rows; j++)
         {
             let div = document.createElement("div");
             div.id = `${i}x${j}`;
-            div.textContent = `${i} x ${j}`;
+            div.style.minWidth = `${size[0]}px`
+            div.style.minHeight = `${size[1]}px`;
+            div.textContent = `${i}x${j}`;
             container.appendChild(div);
         }
     }
@@ -26,15 +38,14 @@ function createDivs(rows, cols)
 
 function hoverEvent(id)
 {
-    console.log(id);
-    //const div = document.getElementById(id);
+    //console.log(id);
+    const div = document.getElementById(id);
     div.style.background = "blue";
 }
 
 function loadPage()
 {
     const container = createDivs(16, 16);
-    console.log(container);
     container.childNodes.forEach(MouseOver => MouseOver.addEventListener("mouseover", function (e) {
         hoverEvent(e.target.id)
     }));
